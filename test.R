639cdac7bda707367f49861e753c2c39d867cd3c
David Kahler <dmkahler@users.noreply.github.com>
2021-08-17 06:48:43 -0400

    added comments.

--- test.R
+++ test.R
@@ -1,11 +1,11 @@
 # This is to test your distribution of R and RStudio
 
-x <- rnorm(1000)
-plot(x)
+x <- rnorm(1000) # This command generates random numbers that are normally distributed.
+plot(x) # This is a built-in plotting tool that will show the values generated.
 
 install.packages("tidyverse")
 library(tidyverse)
 
 y <- data.frame(x)
 ggplot(y) +
-      geom_histogram(aes(x))
+      geom_histogram(aes(x)) # This command is part of ggplot2 and will generate a histogram of the data generated.
